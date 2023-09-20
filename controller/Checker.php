3831fb0ddb670512fce716c41bd14e1b0cb7e5c6
Henrik Heiberg <heiberik@hotmail.com>
2023-09-20 13:34:15 +0200

    wip

--- controller/Checker.php
+++ controller/Checker.php
@@ -2,31 +2,63 @@
 
 namespace oat\udirTestChecker\controller;
 
-use oat\tao\model\service\core_kernel_classes_Resource;
 
 
-class Checker extends \tao_actions_CommonModule {
+class Checker extends \tao_actions_CommonModule
+{
     /**
      * initialize the services
      */
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct();
     }
 
 
-    public function wcagCheck() {
-        $name = '';
+    public function wcagCheck()
+    {
 
-        if ($this->getRequestParameter('name')) {
-            $name = $this->getRequestParameter('name');
+        $testService = \taoTests_models_classes_TestsService::singleton();
+        $qtiTestService = \taoQtiTest_models_classes_QtiTestService::singleton();
+        $itemsService = \taoItems_models_classes_ItemsService::singleton();
+        $qtiItemService = \oat\taoQtiItem\model\qti\Service::singleton();
 
-            $test = new core_kernel_classes_Resource($name);
+        $info = '';
+        $test = new \core_kernel_classes_Resource($this->getRequestParameter('id'));
+
+        // get the items contained in the test
+        $items = $testService->getTestItems($test);
+        $xmlTest = $qtiTestService->getDoc($test);
+
+
+        // iterare over the items
+        foreach ($items as $item) {
+
+
+            //Parse it and build the QTI_Data_Item
+            $file = $qtiItemService->getXmlByRdfItem($item);
+            $qtiParser = new Parser($file);
+            $returnValue = $qtiParser->load();
+
+
+            //$itemXml = $qtiItemService->getXmlByRdfItem($item);
+
+            //$sanitized = \oat\taoQtiItem\helpers\Authoring::sanitizeQtiXml($itemXml);
+            //$qtiItem = $qtiItemService->getXmlToItemParser()->parse($sanitized);
+
+            // convert the XML to a string
+            /*
+            $dom = new \DOMDocument();
+            $dom->preserveWhiteSpace = true;
+            $dom->formatOutput = true;
+            $dom->loadXML($itemXml);
+            $itemXml = $dom->saveXML();
+            */
+
+            $info .= $qtiItem->getBody() . '<br>';
         }
 
 
-        $test = new core_kernel_classes_Resource($this->getRequestParameter('id'));
-
-
-        echo "heeheh";
+        echo $info;
     }
 }
