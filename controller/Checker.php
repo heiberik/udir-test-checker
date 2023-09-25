d77f4edb6a330ff222509da4de7d653db3f06561
heiberik <heiberik@hotmail.com>
2023-09-25 15:55:19 +0200

    getting info

--- controller/Checker.php
+++ controller/Checker.php
@@ -2,7 +2,14 @@
 
 namespace oat\udirTestChecker\controller;
 
-
+/*
+    Potential checks:
+    - maxscore
+    - wcag
+    - correct response set?
+    - pcis in use
+    - normal interactions in use
+*/
 
 class Checker extends \tao_actions_CommonModule
 {
@@ -14,8 +21,68 @@ class Checker extends \tao_actions_CommonModule
         parent::__construct();
     }
 
+    public function checkMaxscore()
+    {
 
-    public function wcagCheck()
+        $testService = \taoTests_models_classes_TestsService::singleton();
+        $qtiTestService = \taoQtiTest_models_classes_QtiTestService::singleton();
+
+        $info = '<h1 style="width: 100%;">Oversikt MAXSCORE:</h1><br>';
+        $test = new \core_kernel_classes_Resource($this->getRequestParameter('id'));
+
+        // get the items contained in the test
+        $items = $testService->getTestItems($test);
+
+        $totalNumberOfTasks = 0;
+        $tasksWithoutMaxscore = 0;
+        $tasksWithOver1Maxscore = 0;
+
+        $tasksWithoutMaxscoreArray = array();
+        $tasksWithOver1MaxscoreArray = array();
+
+        // Iterate over the items
+        foreach ($items as $index => $item) {
+
+            $qtiXmlFileContent = \oat\taoQtiItem\helpers\QtiFile::getQtiFileContent($item);
+            $dom = new \DOMDocument();
+            $dom->loadXML($qtiXmlFileContent);
+            $xpath = new \DOMXPath($dom);
+
+            $label = $item->getLabel();
+
+            $maxScore = (int)$this->getMaxscore($xpath);
+
+            //$info .= "LABEL: " . $label . '<br>';
+            //$info .= "MAXSCORE: " . $maxScore . '<br>';
+
+            $totalNumberOfTasks++;
+            if ($maxScore == 0) {
+                $tasksWithoutMaxscore++;
+                $tasksWithoutMaxscoreArray[] = $label;
+            } else if ($maxScore > 1) {
+                $tasksWithOver1Maxscore++;
+                $tasksWithOver1MaxscoreArray[] = $label;
+            }
+        }
+
+        $info .= "<h2 style='width: 100%;'>Items total:</h2> " . $totalNumberOfTasks . '<br>';
+
+        $info .= "<br><h2 style='width: 100%;'>Items without maxscore:</h2> " . $tasksWithoutMaxscore . '<br>';
+        $info .= "Items:" . '<br>';
+        foreach ($tasksWithoutMaxscoreArray as $string) {
+            $info .= $string . "<br>";
+        }
+
+        $info .= "<br><h2 style='width: 100%;'>Items with maxscore over 1:</h2> " . $tasksWithOver1Maxscore . '<br>';
+        $info .= "Items:" . '<br>';
+        foreach ($tasksWithOver1MaxscoreArray as $string) {
+            $info .= $string . "<br>";
+        }
+
+        echo $info;
+    }
+
+    public function checkWcag()
     {
 
         $testService = \taoTests_models_classes_TestsService::singleton();
@@ -23,7 +90,7 @@ class Checker extends \tao_actions_CommonModule
         $itemsService = \taoItems_models_classes_ItemsService::singleton();
         $qtiItemService = \oat\taoQtiItem\model\qti\Service::singleton();
 
-        $info = 'HHE';
+        $info = '<h1 style="width: 100%;">Udirs prøvesjekker!:</h1> <br><br>';
         $test = new \core_kernel_classes_Resource($this->getRequestParameter('id'));
 
         // get the items contained in the test
@@ -32,44 +99,42 @@ class Checker extends \tao_actions_CommonModule
 
 
         // Iterate over the items
-        foreach ($items as $item) {
-            // Parse the item and build the QTI_Item object
-            $file = $qtiItemService->getXmlByRdfItem($item);
-            $qtiParser = new \oat\taoQtiItem\model\qti\Parser($file);
-            $qtiItem = $qtiParser->load();
-
-            // Get the XML content of the item
-            $itemXML = $qtiItem->toXML();
-
-            //$info .= $itemXML . '<br>';
+        foreach ($items as $index => $item) {
 
 
-            // Use DOMDocument and DOMXPath to search for specific elements in the XML
+            $qtiXmlFileContent = \oat\taoQtiItem\helpers\QtiFile::getQtiFileContent($item);
+            $qtiXmlDoc = new \qtism\data\storage\xml\XmlDocument();
+            $qtiXmlDoc->loadFromString($qtiXmlFileContent);
+
             $dom = new \DOMDocument();
-            $dom->loadXML($itemXML);
+            $dom->loadXML($qtiXmlFileContent);
             $xpath = new \DOMXPath($dom);
 
-            // Example: Find all elements with a specific tag name (replace 'tagName' with the actual tag name)
-            $elements = $xpath->query('//outcomedeclaration');
+            $maxScore = $this->getMaxscore($xpath);
+            $info .= "MAXSCORE: " . $maxScore . '<br>';
 
-            // Iterate over the found elements
-            foreach ($elements as $element) {
-                // Do something with the element, e.g., extract its value
-                $elementValue = $element->nodeValue;
-                $info .= 'Found element with value: ' . $elementValue . '<br>';
-            }
-
-            // Example: Find elements with a specific attribute (replace 'attributeName' and 'attributeValue' with actual values)
-            $elementsWithAttribute = $xpath->query('//outcomedeclaration[@attributeName="MAXSCORE"]');
-
-            // Iterate over the elements with the specified attribute
-            foreach ($elementsWithAttribute as $element) {
-                // Do something with the matching element
-                $info .= 'Found element with attribute: ' . $element->getAttribute('attributeName') . '<br>';
-            }
+            $info .= $qtiXmlFileContent . '<br>';
         }
 
-
         echo $info;
+    }
+
+    public function getMaxscore(\DOMXPath $xpath): string
+    {
+
+        $elements = $xpath->query("*[name(.) = 'outcomeDeclaration']");
+
+        foreach ($elements as $element) {
+
+            $elementValue = $element->nodeValue;
+            $elementIdentifier = $element->getAttribute('identifier');
+
+            if ($elementIdentifier == 'MAXSCORE') {
+                return $elementValue;
+            }
+
+        }
+
+        return "0";
     }
 }
